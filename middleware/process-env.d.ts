320d8065d65bebdc990f2458f89752b0cf582df2
mbeka02 <antonymbeka@gmail.com>
2025-01-14 15:47:50 +0300

    bug fixes

--- middleware/process-env.d.ts
+++ middleware/process-env.d.ts
@@ -1,20 +1,21 @@
+//Process.env always contains strings even for numeric values -Anthony
 declare global {
-    namespace NodeJS {
-        interface ProcessEnv {
-            DATABASE_URL: string,
-            NAIROBI_URL: string,
-            MARIADB_HOST: string,
-            MARIADB_PORT: number,
-            MARIADB_USER: string,
-            MARIADB_PASSWORD: string,
-            MARIADB_CONNECTION_LIMIT: number,
-            MYSQL_HOST: string,
-            MYSQL_USER: string,
-            MYSQL_DATABASE: string,
-            MYSQL_PORT: number,
-            MYSQL_PASSWORD: string
-        }
+  namespace NodeJS {
+    interface ProcessEnv {
+      DATABASE_URL: string;
+      NAIROBI_URL: string;
+      MARIADB_HOST: string;
+      MARIADB_PORT: string;
+      MARIADB_USER: string;
+      MARIADB_PASSWORD: string;
+      MARIADB_CONNECTION_LIMIT: string;
+      MYSQL_HOST: string;
+      MYSQL_USER: string;
+      MYSQL_DATABASE: string;
+      MYSQL_PORT: string;
+      MYSQL_PASSWORD: string;
     }
+  }
 }
 
-export {}
+export { };
--- middleware/src/db/mombasa/db.ts
+++ middleware/src/db/mombasa/db.ts
@@ -1,13 +1,26 @@
-import mysql from "mysql2/promise";
-import "dotenv/config";
+import "dotenv";
+import path from "path";
+import { config } from "dotenv";
 
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+config({ path: path.resolve(__dirname, "../../../.env") });
+import mysql from "mysql2/promise";
 const pool = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    database: process.env.MYSQL_DATABASE,
-    port: process.env.MYSQL_PORT,
-    password: process.env.MYSQL_PASSWORD
-  });
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  database: process.env.MYSQL_DATABASE,
+  port: parseInt(process.env.MYSQL_PORT!, 10),
+  password: process.env.MYSQL_PASSWORD,
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
+});
 
 export default pool;
 
@@ -15,7 +28,7 @@ async function test() {
   try {
     const results = await pool.query("SELECT 1 as val");
     console.log(results);
-  } catch(err) {
+  } catch (err) {
     console.log("MySQL sucks =>", err);
   }
 }
