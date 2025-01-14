320967f80d83f2a0a0380e047750ece85548aa08
mbeka02 <antonymbeka@gmail.com>
2025-01-14 18:24:17 +0300

    added sample data

---
+++ sample_data/Stores.sql
@@ -0,0 +1,3 @@
+insert into Stores (id, name, address) values (1, 'Nairobi', '87651 Merry Circle');
+insert into Stores (id, name, address) values (2, 'Kisumu', '68 Oak Valley Pass');
+insert into Stores (id, name, address) values (3, 'Mombasa', '2042 Iowa Circle');
